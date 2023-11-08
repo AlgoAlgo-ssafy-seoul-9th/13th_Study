b6fea1c5da99998a6772f8bb3197fbd9c6d5b224
임서희 <seooh212@gmail.com>
2023-11-08 21:27:01 +0900

    스티커문제입니다...

---
+++ 스티커/서희.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    lst = [list(map(int, input().split())) for _ in range(2)]
+
+    if n == 1:
+        print(max(lst[0][0], lst[1][0]))
+
+    else:
+        lst[0][1] += lst[1][0]
+        lst[1][1] += lst[0][0]
+        for i in range(2, n):
+            lst[0][i] += max(lst[1][i-1], lst[1][i-2])
+            lst[1][i] += max(lst[0][i-1], lst[0][i-2])
+
+        print(max(lst[0][n-1], lst[1][n-1]))
